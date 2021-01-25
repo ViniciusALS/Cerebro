070ff1cc8862c23db835e8637af3d48a3fd32c44
Vinicius <me@viniciusals.com>
2021-01-25 15:59:41 -0500

    Add comments to main source code

--- Arduino Code/Project/Project.ino
+++ Arduino Code/Project/Project.ino
@@ -8,14 +8,15 @@ RoboticArm myRoboticArm;
 void setup() {
     Serial.begin(9600);
 
-    // Initialize values for robotic arm components
+    // Sets components initial values
     myRoboticArm.begin();
 }
 
 void loop() {
 
     while (Serial.available()) {
-        // Receives either the integer 0, 1, 2, or 3 from serial connection to signify "await", "turnLeft", "turnRight", or "toggle" respectively.
+        // Receives either the integer 0, 1, 2, or 3 from serial connection
+        // to signify "await", "turnLeft", "turnRight", or "toggle" respectively.
         choice = Serial.parseInt();
 
         myRoboticArm.movementChoice(choice);
