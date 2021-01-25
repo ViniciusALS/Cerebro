a86b9a0379ea61f3d1863e37dc95d669a4327178
Vinicius <me@viniciusals.com>
2021-01-25 17:36:40 -0500

    Delete unused code for potentiometers

--- Arduino Code/Project/PINS.h
+++ Arduino Code/Project/PINS.h
@@ -1,12 +1,6 @@
 #ifndef PINS_H
 #define PINS_H
 
-// Define potentiometers pin connections on Arduino
-#define CLAW_POT_PIN            0
-#define FRONT_ARM_POT_PIN       1
-#define BACK_ARM_POT_PIN        2
-#define BOTTOM_POT_PIN          3
-
 // Define servos pin connections on Arduino
 #define CLAW_SERVO_PIN          11
 #define FRONT_ARM_SERVO_PIN     10
