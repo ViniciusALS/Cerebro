32cc454f982c45a8df0cc18f6847123d2c4a5fe1
Vinicius <me@viniciusals.com>
2021-01-25 17:52:20 -0500

    Fix while loop inside of loop

--- Arduino Code/Project/Project.ino
+++ Arduino Code/Project/Project.ino
@@ -7,14 +7,14 @@ RoboticArm myRoboticArm;
 
 void setup() {
     Serial.begin(9600);
-
+ 
     // Sets components initial values
     myRoboticArm.init();
 }
 
 void loop() {
 
-    while (Serial.available()) {
+    if (Serial.available()) {
         // Receives either the integer 0, 1, 2, or 3 from serial connection
         // to signify "await", "turnLeft", "turnRight", or "toggle" respectively.
         choice = Serial.parseInt();
