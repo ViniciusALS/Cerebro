61031d44c1652ffd3615ad799053730f7ec75230
Vinicius <me@viniciusals.com>
2021-01-27 16:43:13 -0500

    Update GUI

--- Brain_Waves_Analysis/Training_Data_Acquisition/GUI/HomePage.py
+++ Brain_Waves_Analysis/Training_Data_Acquisition/GUI/HomePage.py
@@ -4,14 +4,16 @@ class HomePage(Frame):
 	def __init__(self, parent, controller):
 		Frame.__init__(self, parent)
 
-		Label(self, text="Cerebro")
+
+		Label(self, text="Cerebro").grid(column=1, row=1, sticky=(W, S))
 
 
 		teamFrame = Frame(self)
-		teamFrame.grid(column=1, sticky=(W, S))
+		teamFrame.grid(column=1, row=2, sticky=(W, S))
 
 		Label(self, text="A project by:").grid(column=1, row=1, sticky=W)
 		Label(self, text="Vinicius de A. L. e Souza").grid(column=1, row=2, sticky=W)
 		Label(self, text="Yongsun Lin").grid(column=1, row=3, sticky=W)
+
 		
 		
