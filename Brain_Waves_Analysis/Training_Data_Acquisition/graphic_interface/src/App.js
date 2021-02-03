e351ee370f26978b9c92313766e92db5662f5a82
Vinicius <me@viniciusals.com>
2021-02-03 17:46:25 -0500

    Update GUI

--- Brain_Waves_Analysis/Training_Data_Acquisition/graphic_interface/src/App.js
+++ Brain_Waves_Analysis/Training_Data_Acquisition/graphic_interface/src/App.js
@@ -2,24 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Cerebro</h1>
+            </header>
+            <footer className="App-footer">
+                <h2>Made by:</h2>
+                <h3>Vinicius de A. L. e Souza - N01299468</h3>
+                <h3>Yongsun Lim - N01283910</h3>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
