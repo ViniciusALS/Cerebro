2f3ffd8c73353051fc1a1859b22cf8fd3d23aa4b
Vinicius <me@viniciusals.com>
2021-02-17 17:25:37 -0500

    Remove windows menu

--- Brain_Waves_Analysis/Training_Data_Acquisition/graphic_interface/electron/main.js
+++ Brain_Waves_Analysis/Training_Data_Acquisition/graphic_interface/electron/main.js
@@ -8,10 +8,16 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width:800,
         height:600,
-        show: false
+        show: false,
+    	webPreferences: {
+      		nodeIntegration: true
+    	}
     });
+	
     const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
-    mainWindow.loadURL(startURL);
+    
+	mainWindow.removeMenu();
+	mainWindow.loadURL(startURL);
     mainWindow.once('ready-to-show', () => mainWindow.show());
     mainWindow.on('closed', () => {
         mainWindow = null;
